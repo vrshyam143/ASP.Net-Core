61c265e7ff9ad1e8aa9fd8ef9e543fbe96ab94e8
ramanasv <shyam.ramanathan@lexisnexis.com>
2017-10-12 16:31:35 -0400

    Movie search project with upload feature.

---
+++ RazorPagesMovie/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class MovieContext:DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Schedule> Schedule { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
